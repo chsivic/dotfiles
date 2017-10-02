ed9ad50008f3e5afa0a93fb95a3e7ea2f2d2ccc2
victor.chensi@gmail.com <victor.chensi@gmail.com>
2017-10-02 14:29:04 -0700

    cscope dir list, tabstop, filetype python

--- bin/get_cscope_files.sh
+++ bin/get_cscope_files.sh
@@ -1,16 +1,24 @@
 #!/bin/bash
 if [ $# -eq 0 ]
 then
-dir_list=`pwd`
+dir_list=(
+    servicerouter/client/
+    shortwave/
+    dsi/logger/configs/SpeechRecognitionServiceLoggerConfig/
+    tupperware/config/shortwave/
+    language_technology/
+)
 else
 dir_list=$@
 fi
 
 echo "dir_list: ${dir_list[@]}"
-find ${dir_list[@]} -type f  -name "*.c" -o -name "*.cpp" -o -name "*.h" -o -name "*.hpp" \
-        -o -name "*.py" -o -name "*.json" \
-        -o -name "*.thrift" -o -name TARGETS \
-         > cscope.files
+find ${dir_list[@]} -type f  -name "*.c" -o -name "*.cpp" -o -name "*.h" \
+    -o -name "*.hpp" -o -name "*.tcc" \
+    -o -name "*.py" -o -name "*.json" \
+    -o -name "*.thrift" -o -name TARGETS \
+    -o -name "*.cinc" -o -name "*.tw" \
+    > cscope.files
 
 echo "Generating ctags ..."
 ctags -R --c++-kinds=+pl --fields=+iaS --extra=+q -L cscope.files &
